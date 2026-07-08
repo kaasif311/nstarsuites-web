7f8261ef86bd7798757880699063dc873091dc65
Kaasif <kaasif@outlook.com>
2026-07-07 22:25:41 -0400

    Update privacy 3

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -5,49 +5,150 @@ export default function PrivacyPolicyPage() {
 
       <div className="space-y-8 text-slate-700 leading-8">
         <section>
+          <h2 className="mb-3 text-2xl font-semibold">Company Policy</h2>
+          <p>
+            This Privacy Policy describes Winterfell LLC (North Star Business Suites) policies and procedures on the collection, use and
+disclosure of your information when you use our service and tells you about your privacy rights and how
+the law protects you.
+We use your personal data to provide and improve the Service. By using the Service, you agree to the
+collection and use of information in accordance with this Privacy Policy.
+Winterfell LLC (North Star Business Suites) is committed to safeguarding the privacy of our users. We want to assure you that we
+do not share your personal information with third parties. This privacy policy outlines how we collect,
+use, and protect the information you provide to us.
+          </p>
+        </section>
+        <section>
           <h2 className="mb-3 text-2xl font-semibold">Information We Collect</h2>
           <p>
-            North Star Business Suites may collect your name, email address,
-            phone number, company information, and any information you submit
-            through our contact or tour request forms.
+            We collect only the information necessary to provide and improve our services. This may include your
+name, email address and phone number. We do not sell, rent, or share this information with any third
+parties.
           </p>
         </section>
 
         <section>
           <h2 className="mb-3 text-2xl font-semibold">How We Use Information</h2>
           <p>
-            We use your information to respond to inquiries, schedule property
-            tours, provide leasing information, and improve our services.
+            The information collected is used solely for communicating with you as the intended party. We do not
+share or sell your personal information with external parties for marketing or any other purposes.
+Additionally, no mobile or text messaging consent information will be shared with third parties or
+affiliates for marketing or promotional purposes. All the above categories exclude text messaging
+originator opt-in data and consent; this information will not be shared with any third parties.</p>
+<p>
+We may use personal data for the following purposes:
+<ul>
+<li>To provide and maintain our service, including to monitor the usage of our service.</li>
+<li>To manage your Account: to manage your registration as a user of the Service. The Personal Data
+you provide can give you access to different functionalities of the Service that are available to you
+as a registered user.</li>
+<li>For the performance of a contract: the development, compliance and undertaking of the
+purchase contract for the products, items or services you have purchased or of any other contract
+with us through the Service.</li>
+<li>To contact You: To contact you by email, telephone calls, SMS, or other equivalent forms of
+electronic communication, such as a mobile application's push notifications regarding updates
+or informative communications related to the functionalities, products or contracted services,
+including the security updates, when necessary or reasonable for their implementation.</li>
+<li>To provide you with news, special offers and general information about other goods, services and
+events which we offer that are similar to those that you have already purchased or enquired about
+unless you have opted not to receive such information.</li>
+<li>To manage your requests: To attend and manage your requests to us.</li>
+</ul>
+</p><p>All messages you send through the Service, whether to us or other users, are stored on our servers.
+North Star Business Suites employs servers and services owned by third parties to retain these messages.
           </p>
         </section>
 
         <section>
-          <h2 className="mb-3 text-2xl font-semibold">Information Sharing</h2>
+          <h2 className="mb-3 text-2xl font-semibold">DISCLOSURE OF YOUR INFORMATION</h2>
           <p>
-            We do not sell your personal information. Mobile information will not be shared with third parties (including affiliates and vendors) for marketing or promotional purposes. Mobile information will not be shared
-             with third parties for marketing purposes
+            North Star Business Suites does not share any client data with third parties for marketing, promotional purposes,
+or any other purposes. Your personal information is kept confidential and is not disclosed to any outside
+organizations, except as required by law or with your explicit consent. We may disclose your personal
+Information under the following limited circumstances:
+<ul>
+<li>We have obtained your consent.</li>
+<li>We need to enforce our Terms of Service.</li>
+<li>We share information with partners or affiliates that have signed non-disclosure agreements
+with us only to provide you with a specific service.</li>
+<li>We may provide such information to a company controlled by or under common control with
+North Star Business Suites for any purpose allowed by this Policy.</li>
+<li>We respond to subpoenas, court orders, or legal processes, or to establish or exercise our
+legal rights, or the legal rights of others, or defend against legal claims.</li>
+<li>When we believe it is necessary to disclose Personal Information to investigate, prevent, or
+take action regarding illegal activities, suspected fraud, potential threats to anyone's physical
+safety, violations of North Star Business Suites Terms of Service, or as otherwise required by law.</li>
+<li>We transfer Personal Information about you if North Star Business Suites or its assets are acquired by or
+merged with another company.</li>
+</ul>
 
-             SMS Message frequency may vary. SMS Message and data rates may apply. Reply STOP to opt out, HELP for assistance. For support contact 281-270-7299 or support@nstarsuites.com.
+We may share aggregated, non-identifiable information with others without further notice to you, such as
+the total number of people who used the Service in a specific month or the total number of messages
+sent during a particular period.
+
+           
+          </p>
+        </section>
+        <section>
+          <h2 className="mb-3 text-2xl font-semibold">SMS DISCLOSURE</h2>
+          <p>
+            By reaching us by SMS, you agree to receive recurring messages from North Star Business Suites.
+Message and data rates may apply. Message frequency depends on your interactions and preferences.
+You can reply STOP to opt-out of future messages or HELP for more information.
+No mobile or messaging consent information will be shared with third parties for marketing or
+promotional purposes. All the above categories exclude text messaging originator opt-in data and
+consent; this information will not be shared with any third parties.
+          </p>
+        </section>
+        <section>
+            <h2 className="mb-3 text-2xl font-semibold">INTERNATIONAL DATA TRANSFERS</h2>
+            <p>
+            Your Personal Information may be transferred to and processed in locations outside of your state,
+province, country, or other governmental jurisdiction where the data protection laws may differ from
+those in your jurisdiction. We take steps to ensure that your data is handled securely and in line with this
+Policy, regardless of where it is processed.
+            </p>
+        </section>
+        <section>
+          <h2 className="mb-3 text-2xl font-semibold">DATA RETENTION</h2>
+          <p>
+            We retain your Personal Information only as long as necessary to fulfill the purposes outlined in this
+Policy unless a longer retention period is required or permitted by law. We will also retain and use your
+Personal Information as necessary to comply with legal obligations, resolve disputes, and enforce our
+agreements.
+          </p>
+        </section>
+        <section>
+          <h2 className="mb-3 text-2xl font-semibold">COOKIES AND TRACKING TECHNOLOGIES</h2>
+          <p>
+            Our Service may use cookies and similar tracking technologies to enhance your experience. Cookies are
+small text files placed on your device to collect information about your activity on the Service. You can
+control the use of cookies through your browser settings, but disabling cookies may limit your ability to
+use certain features of the page or Service.
           </p>
         </section>
 
         <section>
-          <h2 className="mb-3 text-2xl font-semibold">Cookies</h2>
+          <h2 className="mb-3 text-2xl font-semibold">YOUR CHOICES:</h2>
           <p>
-            This website may use cookies and analytics tools to improve your
-            browsing experience.
+            You have the right to access, correct, or delete your information. If you have any concerns or questions
+about your data, please contact us at:
+<ul>
+<li>281-270-7299</li>
+<li>hello@nstarsuites.com</li>
+<li>2910 Antoine Dr, Suite B100, Houston, TX 77092</li>
+</ul>   
+
           </p>
+
         </section>
-
         <section>
-          <h2 className="mb-3 text-2xl font-semibold">Contact</h2>
+          <h2 className="mb-3 text-2xl font-semibold">CHANGES TO THIS PRIVACY POLICY</h2>
           <p>
-            Questions regarding this Privacy Policy may be sent to:
+            We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new
+Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes.
+Changes to this Privacy Policy are effective when they are posted on this page.
           </p>
-
-          <p className="mt-3 font-semibold">
-            hello@nstarsuites.com
-          </p>
+          <p>Last updated: 7/7/2026</p>
         </section>
       </div>
     </main>
